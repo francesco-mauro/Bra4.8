6bd3ff35a73f4a6711f23d2549e01bb681be06e5
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-24 16:11:06 +0200

    new Constructor

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        Student student1 = new Student("Spongebob", 30, 3.2);
+        Student student2 = new Student("Patrick", 34, 1.5);
+        Student student3 = new Student("Sandy", 27, 4.0);
 
-        Car car1 = new Car();
-        Car car2 = new Car();
+        System.out.println(student1.name);
+        System.out.println(student1.age);
+        System.out.println(student1.gpa);
+        System.out.println(student1.isEnrolled);
 
-        System.out.println(car1.make + " " + car1.model);
-        System.out.println(car2.make + " " + car2.model);
+        System.out.println(student2.name);
+        System.out.println(student2.age);
+        System.out.println(student2.gpa);
+        System.out.println(student2.isEnrolled);
+
+        System.out.println(student3.name);
+        System.out.println(student3.age);
+        System.out.println(student3.gpa);
+        System.out.println(student3.isEnrolled);
+
+        student1.study();
+
+
 
 
     }
