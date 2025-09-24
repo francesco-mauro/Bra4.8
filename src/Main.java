847d9cd521c3caa4fd01e008c2a70d500d22fd4a
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-24 16:47:37 +0200

    new Constructor v.2

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Student student1 = new Student("Spongebob", 30, 3.2);
-        Student student2 = new Student("Patrick", 34, 1.5);
-        Student student3 = new Student("Sandy", 27, 4.0);
+        User user1 = new User("Spongebob");
 
-        System.out.println(student1.name);
-        System.out.println(student1.age);
-        System.out.println(student1.gpa);
-        System.out.println(student1.isEnrolled);
-
-        System.out.println(student2.name);
-        System.out.println(student2.age);
-        System.out.println(student2.gpa);
-        System.out.println(student2.isEnrolled);
-
-        System.out.println(student3.name);
-        System.out.println(student3.age);
-        System.out.println(student3.gpa);
-        System.out.println(student3.isEnrolled);
-
-        student1.study();
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
 
 
 
---
+++ src/User.java
@@ -0,0 +1,14 @@
+public class User {
+
+    String username;
+    String email;
+    int age;
+
+    User(String username){
+        this.username = username;
+        this.email = "not provided";
+        this.age = 0;
+
+    }
+
+}
