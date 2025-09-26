6007c3409a8136287167576c7775bbbbf8851145
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-26 12:02:34 +0200

    New classes foundation

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Person person1 = new Person("Tom" , "Riddle");
-        Student student1 = new Student("Harry", "Potter", 3.25);
-        Employee employee1 = new Employee("Rubeus", "Hagrid", 50000);
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Fish fish = new Fish();
 
-        person1.showName();
-        student1.showName();
-        System.out.println(student1.gpa);
-        System.out.println(student1.first);
 
-        student1.showGPA();
-        employee1.showSalary();
+        dog.move();
+        cat.move();
+        fish.move();
+
     }
 }
