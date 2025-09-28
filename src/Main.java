735112c08fede7d6f6bdfcf90b697b44c133ef44
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-28 16:07:38 +0200

    New Methods

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-        Car car1 = new Car("Ford", "Mustang", 2025, "Red");
-        Car car2= new Car("Chevrolet", "Corvette", 2026, "Blue");
-
-        System.out.println(car1);
-        System.out.println(car2);
-
+    Circle circle = new Circle();
+    Triangle triangle = new Triangle();
+    Rectangle rectangle = new Rectangle();
 
     }
 }
